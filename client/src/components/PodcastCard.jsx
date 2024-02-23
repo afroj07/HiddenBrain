f65867bb53a89ffb38bd17b12cb54aa729c9793c
afroj07 <afrojkhanafroj70@gmail.com>
2024-02-24 02:56:14 +0530

    added components details

--- client/src/components/PodcastCard.jsx
+++ client/src/components/PodcastCard.jsx
@@ -1,7 +1,232 @@
 import React from "react";
+import Avatar from "@mui/material";
+import styled from "styled-components";
+import FavoriteICon from "@mui/icons-material/Favorite";
+import { useState } from "react";
+import { IconButton } from "@mui/material";
+import { favoritePodcast } from "../api";
+import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { openSignin } from "../redux/setSigninSlice";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-const PodcastCard = () => {
-  return <div>PodcastCard</div>;
+const PlayIcon = styled.div`
+  padding: 10px;
+  border-radius: 50%;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  background-color: #9000ff !important;
+  color: white !important;
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  position: absolute !important;
+  top: 45%;
+  right: 10%;
+  display: none;
+  transition: all 0.4s ease-in-out;
+  box-shadow: 0 0 16px 4px #9000ff50 !important;
+`;
+
+const Card = styled(Link)`
+position: relative;
+text-decoration: none;
+background-color: ${({ theme }) => theme.card}; */
+max-width:220px ;
+height: 280px;
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+padding: 16px;
+border-radius: 6px;
+box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
+&:hover{
+  cursor: pointer;
+  transform: translateY(-8px);
+  transition: all 0.4 ease-in-out;
+  box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.3);
+  filter: brightness(1.3);
+}
+&:hover ${PlayIcon}{
+  display: flex;
+}
+`;
+const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 150px;
+`;
+
+const Title = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Description = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_secondary};
+  font-size: 12px;
+`;
+
+const CardImage = styled.img`
+  object-fit: cover;
+  width: 220px;
+  height: 140px;
+  border-radius: 6px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.3);
+  &:hover {
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const CardInfromation = styled.div`
+  display: flex;
+  align-items: flex-end;
+  font-weight: 450;
+  padding: 14px 0px 0px 0px;
+  width: 100%;
+`;
+
+const MainInfo = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 4px;
+`;
+
+const CreatorInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  margin-top: 6px;
+`;
+
+const CreatorName = styled.div`
+  font-size: 12px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+
+const Views = styled.div`
+  font-size: 10px;
+  color: ${({ theme }) => theme.text_secondary};
+  width: max-content;
+`;
+
+const Favorite = styled(IconButton)`
+  color: white;
+  top: 8px;
+  right: 6px;
+  padding: 6px !important;
+  border-radius: 50%;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  position: absolute !important;
+  background: ${({ theme }) => theme.text_secondary + 95} !important;
+  backdrop-filter: blur(4px);
+  box-shadow: 0 0 16px #222423 !important;
+`;
+
+const PodcastCard = ({ podcast, user, setSignInOpen }) => {
+  const [favourite, setFavourite] = useState(false);
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("podstreamtoken");
+
+  const favoritepodcast = async () => {
+    await favoritePodcast(podcast._id, token)
+      .then((res) => {
+        if (res.status === 200) {
+          setFavourite(!favourite);
+        }
+      })
+      .catch((err) => {
+        console, log(err);
+      });
+  };
+
+  React.useEffect(() => {
+    if (user?.favorites?.find((fav) => fav._id === podcast._id)) {
+      setFavourite(true);
+    }
+  }, [user]);
+
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <Card to={`/podcast/${podcast._id}`}>
+      <div>
+        <Top>
+          <Link
+            onClick={() => {
+              if (!currentUser) {
+                dispatch(openSignin());
+              } else {
+                favoritePodcast();
+              }
+            }}
+          >
+            <Favorite>
+              {favourite ? (
+                <FavoriteICon
+                  style={{ color: "#E30022", width: "16px", height: "16px" }}
+                />
+              ) : (
+                <FavoriteICon style={{ width: "16px", height: "16px" }} />
+              )}
+            </Favorite>
+          </Link>
+          <CardImage src={podcast.thumbnail} />
+        </Top>
+        <CardInfromation>
+          <MainInfo>
+            <Title>{podcast.name}</Title>
+            <Description>{podcast.desc}</Description>
+            <CreatorInfo>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "8px" }}
+              >
+                <Avatar
+                  src={podcast.creator.img}
+                  style={{ with: "26px", height: "26px" }}
+                >
+                  {podcast.creator.name?.charAt(0).toUpperCase()}
+                </Avatar>
+                <CreatorName>{podcast.creator.name}</CreatorName>
+              </div>
+              <Views>{podcast.views}</Views>
+            </CreatorInfo>
+          </MainInfo>
+        </CardInfromation>
+      </div>
+      <PlayIcon>
+        {podcast?.type === "video" ? (
+          <PlayArrowIcon style={{ width: "28px", height: "28px" }} />
+        ) : (
+          <HeadphonesIcon style={{ width: "28px", height: "28px" }} />
+        )}
+      </PlayIcon>
+    </Card>
+  );
 };
 
 export default PodcastCard;
--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import styles from "./Signin.module.css";
+import googleIcon from "../Images/google.webp";
+import styled from "styled-components";
 import {
   Block,
   CloseRounded,
@@ -12,16 +15,577 @@ import { IconButton, Modal } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
 import { openSnackbar } from "../redux/snackbarSlice";
-import { UseDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import validator from "validator";
 import {
   signIn,
-  googleSignIn,
+  googleSingIn,
   findUserByEmail,
   resetPassword,
 } from "../api/index";
-const Signin = () => {
-  return <div>Signin</div>;
+
+import OTP from "./OTP.jsx";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { closeSignin } from "../redux/setSigninSlice";
+
+const Signin = ({ setSignInOpen, setSignUpOpen, theme }) => {
+  const OutlineBox = styled.div`
+    height: 44px;
+    border-radius: 12px;
+    border: 1px solid ${({ theme }) => theme.text_secondary};
+    color: ${({ theme }) => theme.text_secondary};
+    ${({ googleButton, theme }) =>
+      googleButton &&
+      `user-select:none;
+  gap:16px  `}
+
+    ${({ button, theme }) =>
+      button &&
+      `user-select:none;
+border:none;
+background:${theme.button};
+color:${theme.bg};
+`}
+${({ activeButton, theme }) =>
+      activeButton &&
+      ` user-select:none;
+  border:none;
+  background:${theme.primary};
+  color:white;
+  `}
+  margin:3px 20px;
+    font-size: 14px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 500;
+    padding: 0px 14px;
+  `;
+
+  const Error = styled.div`
+    color: red;
+    font-size: 10px;
+    margin: 2px 26px 8px 26px;
+    display: block;
+    ${({ error, theme }) => error === "" && `display: none;`}
+  `;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [Loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [values, setValues] = useState({
+    password: "",
+    showPassword: false,
+  });
+  // verify otp
+
+  const [showOTP, setShowOTP] = useState(false);
+  const [otpVerified, setOtpVerified] = useState(false);
+
+  //resetPassword
+  const [showForgetPassword, setShowForgetPassword] = useState(false);
+  const [samepassword, setSamepassword] = useState("");
+  const [newpassword, setNewpassword] = useState("");
+  const [confirmedpassword, setConfirmedpassword] = useState("");
+  const [passwordCorrect, setPasswordCurrect] = useState(false);
+  const [resetDisabled, setResetDisabled] = useState(true);
+  const [resettingPassword, setResettingPassword] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (email !== "") validateEmail();
+    if (validator.isEmail(email) && password.length > 5) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!disabled) {
+      dispatch(loginStart());
+      setDisabled(true);
+      setLoading(true);
+      try {
+        signIn({ email, password }).then((res) => {
+          if (res.status === 200) {
+            dispatch(loginSuccess(res.data));
+            setLoading(false);
+            setDisabled(false);
+            dispatch(closeSignin());
+            dispatch(
+              openSnackbar({
+                message: "Logged In Successfully",
+                severity: "success",
+              })
+            );
+          } else if (res.status === 203) {
+            dispatch(loginFailure());
+            setLoading(false);
+            setDisabled(false);
+            dispatch(
+              openSnackbar({
+                message: "Account Not Verified",
+                severity: "error",
+              })
+            );
+          } else {
+            dispatch(loginFailure());
+            setLoading(false);
+            setDisabled(false);
+            setcredentialError(`Invalid Credential : ${res.data.message}`);
+          }
+        });
+      } catch (err) {
+        dispatch(loginFailure());
+        setLoading(false);
+        setDisabled(false);
+        dispatch(
+          openSnackbar({
+            message: err.message,
+            severity: "error",
+          })
+        );
+      }
+    }
+
+    if (email === "" || password === "") {
+      dispatch(
+        openSnackbar({
+          message: "Please fill all the fields",
+          severity: "error",
+        })
+      );
+    }
+  };
+
+  const [emailError, setEmailError] = useState("");
+  const [credentialError, setcredentialError] = useState("");
+
+  const validateEmail = () => {
+    if (validator.isEmail(email)) {
+      setEmailError("");
+    } else {
+      setEmailError("Enter a valid Email Id!");
+    }
+  };
+
+  //validate password
+  const validatePassword = () => {
+    if (newpassword.length < 8) {
+      setSamepassword("Password must be atleast 8 chaacters long!");
+      setPasswordCurrect(false);
+    } else if (newpassword.length > 16) {
+      setSamepassword("Passord must be less than 16 characters long!");
+      setPasswordCurrect(false);
+    } else if (
+      !newpassword.match(/[a-z]/g) ||
+      !newpassword.match(/[A-Z]/g) ||
+      !newpassword.match(/[0-9]/g) ||
+      !newpassword.match(/[^a-zA-Z\d]/g)
+    ) {
+      setPasswordCurrect(false);
+      setSamepassword(
+        "Password must contain atleast one lowercase, uppercase,number and special character!"
+      );
+    } else {
+      setSamepassword("");
+      setPasswordCurrect(true);
+    }
+  };
+
+  useEffect(() => {
+    if (newpassword !== "") validatePassword();
+    if (passwordCorrect && newpassword === confirmedpassword) {
+      setSamepassword("");
+      setResetDisabled(false);
+    } else if (confirmedpassword !== "" && passwordCorrect) {
+      setSamepassword("Password do not match!");
+      setResetDisabled(true);
+    }
+  }, [newpassword, confirmedpassword]);
+
+  const sendOtp = () => {
+    if (!resetDisabled) {
+      setResetDisabled(true);
+      setLoading(true);
+      findUserByEmail(email)
+        .then((res) => {
+          if (res.status === 200) {
+            setShowOTP(true);
+            setResetDisabled(false);
+            setLoading(false);
+          } else if (res.status === 202) {
+            setEmailError("User not found!");
+            setResetDisabled(false);
+            setLoading(false);
+          }
+        })
+        .catch((err) => {
+          setResetDisabled(false);
+          setLoading(false);
+          dispatch(
+            openSnackbar({
+              message: err.message,
+              severity: "error",
+            })
+          );
+        });
+    }
+  };
+
+  const performResetPassword = async () => {
+    if (otpVerified) {
+      setShowOTP(false);
+      setResettingPassword(true);
+
+      await resetPassword(email, confirmedpassword)
+        .then((res) => {
+          if (res.status === 200) {
+            dispatch(
+              openSnackbar({
+                message: "Password Reset Successfully",
+                severity: "success",
+              })
+            );
+            setShowForgetPassword(false);
+            setEmail("");
+            setNewpassword("");
+            setConfirmedpassword("");
+            setOtpVerified(false);
+            setResettingPassword(false);
+          }
+        })
+        .catch((err) => {
+          dispatch(
+            openSnackbar({
+              message: err.message,
+              severity: "error",
+            })
+          );
+          setShowOTP(false);
+          setOtpVerified(false);
+          setResettingPassword(false);
+        });
+    }
+  };
+
+  const closeForgetPassword = () => {
+    setShowForgetPassword(false);
+    setShowOTP(false);
+  };
+  useEffect(() => {
+    performResetPassword();
+  }, [otpVerified]);
+
+  //Google SignIn
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      setLoading(true);
+      const user = await axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+        })
+        .catch((err) => {
+          disabled(loginFailure());
+          dispatch(
+            openSnackbar({
+              message: err.message,
+              severity: "error",
+            })
+          );
+        });
+      googleSingIn({
+        name: user.data.name,
+        email: user.data.email,
+        img: user.data.picture,
+      }).then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          dispatch(loginSuccess(res.data));
+          dispatch(closeSignin());
+          dispatch(
+            openSnackbar({
+              message: "Logged In Successfully",
+              severity: "success",
+            })
+          );
+          setLoading(false);
+        } else {
+          dispatch(loginFailure(res.data));
+          dispatch(
+            openSnackbar({
+              message: res.data.message,
+              severity: "error",
+            })
+          );
+          setLoading(false);
+        }
+      });
+    },
+
+    onError: (errorResponse) => {
+      dispatch(loginFailure());
+      setLoading(false);
+      dispatch(
+        openSnackbar({
+          message: errorResponse.error,
+          severity: "error",
+        })
+      );
+    },
+  });
+
+  return (
+    <Modal open={true} onClose={() => dispatch(closeSignin())}>
+      <div className={styles.container}>
+        {!showForgetPassword ? (
+          <div className={styles.wrapper}>
+            <CloseRounded
+              style={{
+                position: "absolute",
+                top: "24px",
+                right: "30px",
+                cursor: "pointer",
+                color: "whitesmoke",
+              }}
+              onClick={() => dispatch(closeSignin())}
+            />
+            <>
+              <div className={styles.title}>Sign In</div>
+              <OutlineBox
+                googleButton={TroubleshootRounded}
+                style={{ margin: "24px" }}
+                onClick={() => googleLogin()}
+              >
+                {Loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : (
+                  <>
+                    <img style={{ width: "22px" }} src={googleIcon} />
+                    Sign In with Google
+                  </>
+                )}
+              </OutlineBox>
+              <div className={styles.divider}>
+                <div className={styles.line}>or</div>
+              </div>
+              <OutlineBox style={{ marginTop: "24px" }}>
+                <EmailRounded
+                  sx={{ fontSize: "20px" }}
+                  style={{ paddingRight: "12px" }}
+                />
+                <input
+                  className={styles.textInput}
+                  type="email"
+                  placeholder="Email Id"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </OutlineBox>
+              <Error error={emailError}>{emailError}</Error>
+              <OutlineBox>
+                <PasswordRounded
+                  sx={{ fontSize: "20px" }}
+                  style={{ paddingRight: "12px" }}
+                />
+                <input
+                  className={styles.textInput}
+                  type={values.showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <IconButton
+                  color="inherit"
+                  onClick={() =>
+                    setValues({ ...values, showPassword: !values.showPassword })
+                  }
+                >
+                  {values.showPassword ? (
+                    <Visibility sx={{ fontSize: "20px" }} />
+                  ) : (
+                    <VisibilityOff sx={{ fontSize: "20px" }} />
+                  )}
+                </IconButton>
+              </OutlineBox>
+              <Error error={credentialError}>{credentialError}</Error>
+              <div
+                className={styles.forgetPassword}
+                onClick={() => {
+                  setShowForgetPassword(true);
+                }}
+              >
+                <b>Forget Password</b>
+              </div>
+              <OutlineBox
+                button={"true"}
+                activeButton={!disabled}
+                style={{ marginTop: "6px", cursor: "pointer" }}
+                onClick={handleLogin}
+              >
+                {Loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : (
+                  "Sign In"
+                )}
+              </OutlineBox>
+            </>
+            <div className={styles.loginText}>
+              Don't have an account ?
+              <span
+                className={styles.span}
+                onClick={() => {
+                  setSignUpOpen(true);
+                  dispatch(closeSignin());
+                }}
+                style={{
+                  fontWeight: "500",
+                  marginLeft: "6px",
+                  cursor: "pointer",
+                }}
+              >
+                Create Account
+              </span>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.wrapper}>
+            <CloseRounded
+              style={{
+                position: "absolute",
+                top: "24px",
+                right: "30px",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                closeForgetPassword();
+              }}
+            />
+            {!showOTP ? (
+              <>
+                <div className={styles.title}>Reset Password</div>
+                {resettingPassword ? (
+                  <div
+                    style={{
+                      padding: "12px 26px",
+                      marginBottom: "20px",
+                      textAlign: "center",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      gap: "14px",
+                      justifyContent: "center",
+                    }}
+                  >
+                    Updating Password
+                    <CircularProgress color="inherit" size={20} />
+                  </div>
+                ) : (
+                  <>
+                    <OutlineBox style={{ marginTop: "24px" }}>
+                      <EmailRounded
+                        sx={{ fontSize: "20px" }}
+                        style={{ paddingRight: "12px" }}
+                      />
+                      <input
+                        className={styles.textInput}
+                        placeholder="Email ID"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </OutlineBox>
+                    <Error error={emailError}>{emailError}</Error>
+
+                    <OutlineBox>
+                      <PasswordRounded
+                        sx={{ fontSize: "20px" }}
+                        style={{ paddingRight: "12px" }}
+                      />
+                      <input
+                        className={styles.textInput}
+                        placeholder="New Password"
+                        type="text"
+                        onChange={(e) => setNewpassword(e.target.value)}
+                      />
+                    </OutlineBox>
+                    <OutlineBox>
+                      <PasswordRounded
+                        sx={{ fontSize: "20px" }}
+                        style={{ paddingRight: "12px" }}
+                      />
+                      <input
+                        className={styles.textInput}
+                        placeholder="Confirm Password"
+                        type={values.showPassword ? "text" : "password"}
+                        onChange={(e) => setConfirmedpassword(e.target.value)}
+                      />
+                      <IconButton
+                        color="inherit"
+                        onClick={() =>
+                          setValues({
+                            ...values,
+                            showPassword: !values.showPassword,
+                          })
+                        }
+                      >
+                        {values.showPassword ? (
+                          <Visibility sx={{ fontSize: "20px" }} />
+                        ) : (
+                          <VisibilityOff sx={{ fontSize: "20px" }} />
+                        )}
+                      </IconButton>
+                    </OutlineBox>
+                    <Error error={samepassword}>{samepassword}</Error>
+
+                    <OutlineBox
+                      button={"true"}
+                      activeButton={!resetDisabled}
+                      style={{ marginTop: "6px", marginBottom: "24px" }}
+                      onClick={() => sendOtp()}
+                    >
+                      {Loading ? (
+                        <CircularProgress color="inherit" size={24} />
+                      ) : (
+                        "Submit"
+                      )}
+                    </OutlineBox>
+                    <div className={styles.loginText}>
+                      Don'nt have an account ?
+                      <span
+                        className={styles.span}
+                        onClick={() => {
+                          setSignUpOpen(true);
+                          dispatch(closeSignin());
+                        }}
+                        style={{
+                          fontWeight: "500",
+                          marginLeft: "6px",
+                          cursor: "pointer",
+                        }}
+                      >
+                        Create Account
+                      </span>
+                    </div>
+                  </>
+                )}
+              </>
+            ) : (
+              <OTP
+                email={email}
+                name="User"
+                otpVerified={otpVerified}
+                setOtpVerified={setOtpVerified}
+                reason="FORGOTPASSWORD"
+              />
+            )}
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
 };
 
 export default Signin;
